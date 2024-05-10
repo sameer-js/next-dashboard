d3ba292f9b87a28eb9756f2352167168331293c2
Sameer <meersj@gmail.com>
2024-05-10 14:50:22 +0545

    added Search component

---
+++ app/components/dashboard/search/search.jsx
@@ -0,0 +1,17 @@
+import styles from './search.module.css';
+import { MdSearch } from 'react-icons/md';
+
+const Search = ({ placeholder }) => {
+  return (
+    <div className={styles.container}>
+      <MdSearch />
+      <input
+        type='text'
+        placeholder={`Search for ${placeholder}`}
+        className={styles.input}
+      />
+    </div>
+  );
+};
+
+export default Search;
