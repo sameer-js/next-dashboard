9e90a93876240fd53d8170b75e09e7cac23de951
Sameer <meersj@gmail.com>
2024-05-14 14:45:56 +0545

    minor changes

--- app/dashboard/layout.jsx
+++ app/dashboard/layout.jsx
@@ -1,7 +1,7 @@
 import Navbar from '../components/dashboard/navbar/navbar';
 import Sidebar from '../components/dashboard/sidebar/sidebar';
 import styles from '../components/dashboard/dashboard.module.css';
-// import Footer from "../components/dashboard/footer/footer"
+import Footer from '../components/dashboard/footer/footer';
 
 const Layout = ({ children }) => {
   return (
@@ -12,7 +12,7 @@ const Layout = ({ children }) => {
       <div className={styles.content}>
         <Navbar />
         {children}
-        {/* <Footer/> */}
+        <Footer />
       </div>
     </div>
   );
