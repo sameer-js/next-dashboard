186deee77ddf410a1b7f1b0646adcb6eb4379e31
Sameer <meersj@gmail.com>
2024-05-14 14:44:50 +0545

    added Login component

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -1,5 +1,12 @@
-const Login = () => {
-  return <div>Login</div>;
+import styles from '@/app/components/login/login.module.css';
+import LoginForm from '../components/login/loginForm/loginForm';
+
+const LoginPage = () => {
+  return (
+    <div className={styles.container}>
+      <LoginForm />
+    </div>
+  );
 };
 
-export default Login;
+export default LoginPage;
